1f2b3d5f81a1ed3c1f099b15d0cc1a3b80c22b5f
Szymon Patora <szymonopatora@gmail.com>
2019-07-07 23:39:51 +0200

    pom.xml updated

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Owner.java
@@ -1,11 +1,7 @@
 package guru.springframework.petclinic.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Owner extends Person {
 }
