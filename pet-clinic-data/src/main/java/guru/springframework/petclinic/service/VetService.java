d15eb1bb49acaf491b785f38bca41a9734761b5e
Szymon Patora <szymonopatora@gmail.com>
2019-07-08 16:54:44 +0200

    Services refactored. Closes #11

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/service/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/service/VetService.java
@@ -2,13 +2,6 @@ package guru.springframework.petclinic.service;
 
 import guru.springframework.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet pet);
-
-    Set<Vet> findAll();
 }
