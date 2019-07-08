6e1a6d0374b6c8a7c3af5adc982c0f127778da4a
Szymon Patora <szymonopatora@gmail.com>
2019-07-08 15:03:11 +0200

    Services create. closes issue #5

---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/service/VetService.java
@@ -0,0 +1,14 @@
+package guru.springframework.petclinic.service;
+
+import guru.springframework.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet pet);
+
+    Set<Vet> findAll();
+}
--- pet-clinic-web/src/test/java/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/petclinic/PetClinicApplicationTests.java
@@ -11,6 +11,7 @@ public class PetClinicApplicationTests {
 
     @Test
     public void contextLoads() {
+        //TODO
     }
 
 }
