aab41d18fe93df5c7fd71c36e112187cd0d83372
Szymon Patora <szymonopatora@gmail.com>
2019-07-12 11:46:47 +0200

    Added PetTypes in Bootstrap

--- pet-clinic-web/src/main/java/guru/springframework/petclinic/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/guru/springframework/petclinic/bootstrap/DataInitializer.java
@@ -1,8 +1,10 @@
 package guru.springframework.petclinic.bootstrap;
 
 import guru.springframework.petclinic.model.Owner;
+import guru.springframework.petclinic.model.PetType;
 import guru.springframework.petclinic.model.Vet;
 import guru.springframework.petclinic.service.OwnerService;
+import guru.springframework.petclinic.service.PetTypeService;
 import guru.springframework.petclinic.service.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,15 +15,25 @@ public class DataInitializer implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     @Autowired
-    public DataInitializer(OwnerService ownerService, VetService vetService) {
+    public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Marcjanna");
