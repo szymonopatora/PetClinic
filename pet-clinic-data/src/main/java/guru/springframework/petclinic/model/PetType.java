fe4a96310a6cbe31ee1ee074c2a80462ad9e5d8a
Szymon Patora <szymonopatora@gmail.com>
2019-07-22 12:15:06 +0200

    Owner switched to JPA. Closes #35

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/PetType.java
@@ -4,11 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
 
 }
