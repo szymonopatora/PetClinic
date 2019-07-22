2f7e96abe8b49fcc8c3c64c8c57ddc8b823f9ee6
Szymon Patora <szymonopatora@gmail.com>
2019-07-22 12:36:05 +0200

    Pet and Visit converted to JPA

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Visit.java
@@ -4,14 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Visist extends BaseEntity{
+@Entity
+@Table(name = "visits")
+public class Visit extends BaseEntity{
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 }
