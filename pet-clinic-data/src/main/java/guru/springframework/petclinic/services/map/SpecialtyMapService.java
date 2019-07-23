b9bbb2ef047242faed3c8c83d2ae0cd1e41b084d
Szymon Patora <szymonopatora@gmail.com>
2019-07-23 21:19:41 +0200

    Profile in MapServices added

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/SpecialtyMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/SpecialtyMapService.java
@@ -2,11 +2,13 @@ package guru.springframework.petclinic.services.map;
 
 import guru.springframework.petclinic.model.Specialty;
 import guru.springframework.petclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
     @Override
     public Set<Specialty> findAll() {
